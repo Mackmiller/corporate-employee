7ed3b170af41b79203edc3cfb9bb2b42c52ec488
Mackmiller <mackmiller19@gmail.com>
2021-10-23 12:41:40 -0400

    added img folder and scenario object

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//object
+const scenarioInfo = {
+    //scenario 1
+    scenario1 : { 
+     scenario: "It's Sunday night. Do you have Sunday scaries about work tomorrow?",
+     //a = positive, b = negative
+     a: "No, feeling good about tomorrow.",
+     b: "Yes, feeling anxious about having work tomorrow."
+     },
+     //scenario 2
+     scenario2 : { 
+     scenario: "It's Monday morning. Do you indulge your boss when they ask how your weekend was?",
+     a: "I give them an overview of my weekend and even ask about theirs.",
+     b: "I barely respond and move on to work-related topics."
+     }
+ }
+ 
+ console.log(scenarioInfo.scenario2.b)
