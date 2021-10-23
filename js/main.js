1e5cebce2d4dd66641f262fbb20244568000988a
Mackmiller <mackmiller19@gmail.com>
2021-10-23 13:32:43 -0400

    functional event listener on start button to scenario 0

--- js/main.js
+++ js/main.js
@@ -15,9 +15,37 @@
 //      }
 //  }
  
-// went with array
+//DECLARE VARIABLES
+//set up arrays
 var scenarioInfo = new Array ();
 scenarioInfo[0] = new Array ("It's Sunday night. Do you have Sunday scaries about work tomorrow?", "No", "Yes")
 scenarioInfo[1] = new Array("It's Monday morning. Do you indulge your boss when they ask how your weekend was?", "I give an in-depth overview and even ask about theirs.", "I quickly move on to work-related conversation.", 8)
+//reset count
+let count = 0
+//FUNCTIONS
 
-console.log(scenarioInfo[1][3]) //console longs the time correctly
+const populateScenarioBox = () => {
+    let scenario = document.querySelector("#scenarioBox")
+    if (count == 1) {
+        scenario.innerHTML = scenarioInfo[0][0]
+    }
+    //scenario.textContent = scenarioInfo[0][0]
+}
+//total score function
+//start game function
+//reset game function
+
+form.addEventListener ("submit", populateScenarioBox)
+
+//ONCE PAGE LOADS
+document.addEventListener ("DOMContentLoaded", () => {
+
+    //when start button is pressed, change text to scenario 0
+    form.addEventListener ("submit", (e) => {
+        e.preventDefault()
+        count++
+        console.log(count)
+        populateScenarioBox()
+    })
+
+})
