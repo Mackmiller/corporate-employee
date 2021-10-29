ce015366535a6e6d8c03d7dc7b6a9e98d5f9f7ba
Mackmiller <mackmiller19@gmail.com>
2021-10-29 09:04:41 -0400

    additional debugging

--- js/main.js
+++ js/main.js
@@ -379,7 +379,7 @@ document.addEventListener ("DOMContentLoaded", () => {
             const elem12 = document.querySelector("#modal12")
             const instance12 = M.Modal.init(elem12, {dismissible: false})
             instance12.open()
-            //resetGame()
+            resetGame()
         } else {
             response2.href = "#modal7"
             const elem12 = document.querySelector("#modal12")
